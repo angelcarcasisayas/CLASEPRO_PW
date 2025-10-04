1d9390fb3b5ae9dc50512cf491b63ac76adc288a
angelcarcasisayas <angel.carcasi.sayas@gmail.com>
2025-10-04 19:16:51 +0200

    si

--- T1/ProyectoInicial/src/Main.java
+++ T1/ProyectoInicial/src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+
+    // practica
+    //TODO esta pendiente
+    public static void main(String[] args){
+        System.out.println("Este es mi primer programa en Java\n");
+        System.out.println("Esta es la segunda linea del programa");
+    }
+
+    public void nombreMetodo(){
+
+    }
 }
