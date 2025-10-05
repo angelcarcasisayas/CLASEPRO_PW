3fcb2903dc91737b8d297c8f3c02b970e325b5a9
angelcarcasisayas <angel.carcasi.sayas@gmail.com>
2025-10-05 20:43:16 +0200

    si

--- T1/INICIAL/src/Main.java
+++ T1/INICIAL/src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(){
+        System.out.print("\n\nHola mundo");
+        System.out.print("\n\nHola universo");
+        System.out.print("\n\nHola humanos que bien que estan aqui por que me preocupaba mucho que solo estubieramos " +
+                "aliens ajajjaj");
+        System.out.print("\n\nComo son las cosas no? talvez esperaba algo diferente de estar aqui solo porque " +
+                " me preocupa mucho el bienestar de todos al mi alrededor talvez estaba con la cabeza fuera de su lugar");
+    }
+
 }
